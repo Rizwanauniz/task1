f37cfdf0405b53766991c030ce25adc2a3488c0f
Rizwana Uniz <rizwanauniz@gmail.com>
2024-05-29 16:31:51 +0530

    first commit

---
+++ src/com/test/FlowControl.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class FlowControl {
+	
+	public static void main(String[] args) {
+		int a=10, b=20;
+		
+		if(true) {
+			System.out.println(a);
+			System.out.println(b);
+			System.out.println(a+b);
+		}
+	}
+
+}
